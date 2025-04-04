93d90393520245e8b24c2b4f70d03ab3982f4ffb
niw5924 <niw5924@naver.com>
2025-04-05 04:12:17 +0900

    코딩테스트 초급 5일차

---
+++ lib/levels/beginner/day_5_news_app.dart
@@ -0,0 +1,79 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Day5Page extends StatefulWidget {
+  const Day5Page({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _Day5PageState();
+}
+
+class _Day5PageState extends State<Day5Page> {
+  List<Map<String, dynamic>> newsList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadNews();
+  }
+
+  Future<void> loadNews() async {
+    final String jsonString =
+        await rootBundle.loadString('assets/day_5_news_data.json');
+
+    setState(() {
+      newsList = jsonDecode(jsonString).cast<Map<String, dynamic>>();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("뉴스")),
+      body: newsList.isEmpty
+          ? Center(child: const CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: newsList.length,
+              itemBuilder: (context, index) {
+                final news = newsList[index];
+
+                return Card(
+                  child: ListTile(
+                    title: Text(news['title']),
+                    subtitle: Text(news['summary']),
+                    onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (_) => NewsDetailPage(news: news))),
+                  ),
+                );
+              },
+            ),
+    );
+  }
+}
+
+class NewsDetailPage extends StatelessWidget {
+  final Map<String, dynamic> news;
+
+  const NewsDetailPage({
+    super.key,
+    required this.news,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(news['title'])),
+      body: Column(
+        children: [
+          Text(news['author']),
+          const SizedBox(height: 12),
+          Text(news['content']),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_coding_test_practice/levels/beginner/day4_gcd_calculator_page.dart';
+import 'package:flutter_coding_test_practice/levels/beginner/day_5_news_app.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.blue),
 
       /// 이 부분만 바꿔야 함.
-      home: Day4Page(),
+      home: Day5Page(),
     );
   }
 }
