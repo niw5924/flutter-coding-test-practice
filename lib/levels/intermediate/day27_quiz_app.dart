a8f4b80bbff3cbf46674686b9297958840d2ac5c
niw5924 <niw5924@naver.com>
2025-09-20 01:36:00 +0900

    코딩테스트 중급 27일차

--- lib/levels/intermediate/day27_quiz_app.dart
+++ lib/levels/intermediate/day27_quiz_app.dart
@@ -12,16 +12,58 @@ class Day27Page extends StatefulWidget {
 
 class _Day27PageState extends State<Day27Page> {
   List<dynamic> quizList = [];
+  QuizItem? currentQuiz;
+  String? selectedAnswer;
+  int score = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    init();
+  }
+
+  Future<void> init() async {
+    await loadQuiz();
+    startQuiz();
+  }
 
   Future<void> loadQuiz() async {
     final jsonString = await rootBundle.loadString('assets/day27_quiz.json');
 
     final quiz = jsonDecode(jsonString);
     print(quiz.runtimeType);
+
     setState(() {
       quizList = quiz.map((e) => QuizItem.fromJson(e)).toList()..shuffle();
     });
-    print(quizList);
+  }
+
+  void startQuiz() async {
+    if (quizList.isEmpty) {
+      currentQuiz = null;
+      return;
+    }
+
+    setState(() {
+      currentQuiz = quizList.removeAt(0);
+      print(quizList);
+      print(currentQuiz);
+    });
+  }
+
+  void submit() async {
+    if (currentQuiz!.answer == selectedAnswer) {
+      print('정답입니다.');
+      setState(() {
+        score += 1;
+      });
+    } else {
+      print('오답입니다.');
+    }
+
+    setState(() {
+      selectedAnswer = null;
+    });
   }
 
   @override
@@ -31,39 +73,80 @@ class _Day27PageState extends State<Day27Page> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            ElevatedButton(
-              onPressed: loadQuiz,
-              child: const Text('시작'),
-            ),
-            ListView.separated(
-              separatorBuilder: (_, __) => const Divider(),
-              physics: NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: quizList.length,
-              itemBuilder: (context, index) {
-                final quizItem = quizList[index];
-                final options = quizItem.options;
+            if (currentQuiz != null) ...[
+              Text(currentQuiz!.question),
+              const SizedBox(height: 12),
+              ListView.separated(
+                separatorBuilder: (_, __) => const SizedBox(height: 8),
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: currentQuiz!.options.length,
+                itemBuilder: (context, index) {
+                  final options = currentQuiz!.options[index];
 
-                return Column(
-                  children: [
-                    Text(quizItem.question),
-                    ListView.separated(
-                      separatorBuilder: (_, __) => const SizedBox(height: 6),
-                      physics: NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      itemCount: options.length,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          color: Colors.grey,
-                          padding: const EdgeInsets.all(8),
-                          child: Text(options[index]),
-                        );
-                      },
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedAnswer = options;
+                      });
+                      print(selectedAnswer);
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        border: Border.all(
+                          color: selectedAnswer == options
+                              ? Colors.blue
+                              : Colors.transparent,
+                          width: 3.0,
+                        ),
+                      ),
+                      child: Text(options),
                     ),
-                  ],
-                );
-              },
-            ),
+                  );
+                },
+              ),
+              const SizedBox(height: 12),
+              if (selectedAnswer != null)
+                ElevatedButton(
+                  onPressed: () {
+                    submit();
+                    startQuiz();
+                  },
+                  child: const Text('제출'),
+                ),
+            ],
+            Text('$score / 5'),
+            // ListView.separated(
+            //   separatorBuilder: (_, __) => const Divider(),
+            //   physics: NeverScrollableScrollPhysics(),
+            //   shrinkWrap: true,
+            //   itemCount: quizList.length,
+            //   itemBuilder: (context, index) {
+            //     final quizItem = quizList[index];
+            //     final options = quizItem.options;
+            //
+            //     return Column(
+            //       children: [
+            //         Text(quizItem.question),
+            //         ListView.separated(
+            //           separatorBuilder: (_, __) => const SizedBox(height: 6),
+            //           physics: NeverScrollableScrollPhysics(),
+            //           shrinkWrap: true,
+            //           itemCount: options.length,
+            //           itemBuilder: (context, index) {
+            //             return Container(
+            //               color: Colors.grey,
+            //               padding: const EdgeInsets.all(8),
+            //               child: Text(options[index]),
+            //             );
+            //           },
+            //         ),
+            //       ],
+            //     );
+            //   },
+            // ),
           ],
         ),
       ),
